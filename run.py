525b27e8a39323b1b7c2d67424dc4c0cffac4c46
TristanMQ <tristan.tung@students.mq.edu.au>
2025-05-08 10:54:26 +1000

    Some minor changes to the auth, and changed the debug from T to F in run

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from project import create_app
 
 if __name__ == '__main__':
   app = create_app()
-  app.run(host = '127.0.0.0', port = 8001, debug=True) #DO  NOT USE 0.0.0.0 its a f###### open address
+  app.run(host = '127.0.0.0', port = 8001, debug=False) #DO  NOT USE 0.0.0.0 its a f###### open address
 
